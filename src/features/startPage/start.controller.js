bd19b332006efab30379f3463fa714759a0f5930
beatabubula <beatabubula@interia.pl>
2017-05-17 22:15:27 +0200

    save image in project dir

--- src/features/startPage/start.controller.js
+++ src/features/startPage/start.controller.js
@@ -1,11 +1,11 @@
 export default function StartController($scope,$interval) {
-    $scope.image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhnDdt3T3qDnRgSFyR8rNNrqbtEzK1r_DhD3YsLgeYcqOcchleiw"
+    $scope.image = "images/beer1.jpg"
     $scope.number = 0;
     $interval(function() {
         $scope.number++;
     }, 2000);
 
-    var images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhnDdt3T3qDnRgSFyR8rNNrqbtEzK1r_DhD3YsLgeYcqOcchleiw","http://davisbeerweek.com/wp-content/uploads/2016/06/Draft-Beer-1500x850.jpg","http://media.medicalbag.com/images/2016/02/09/evolution-of-craft-beers.jpg"];
+    var images = ["images/beer1.jpg","images/beer2.jpg","images/beer3.jpg"];
     $scope.imageClass = function() {
         $scope.image = images[$scope.number % images.length];
     };
