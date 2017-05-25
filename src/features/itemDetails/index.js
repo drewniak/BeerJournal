86f08499920ce0f4d2c0aa3bc27fca2573ef00a4
beatabubula <beatabubula@interia.pl>
2017-05-25 18:56:07 +0200

    back to star icons in rating

--- src/features/itemDetails/index.js
+++ src/features/itemDetails/index.js
@@ -3,12 +3,11 @@ import uirouter from 'angular-ui-router';
 import 'angular-material';
 import 'angular-route'
 import 'angular-jk-rating-stars';
-import 'angular-rating-icons';
 
 import routing from './itemDetails.routes';
 import itemDetailsController from './itemDetails.controller';
 
-export default angular.module('itemDetailsController', [uirouter, 'ngMaterial', 'ngRoute','jkAngularRatingStars','angular-rating-icons'])
+export default angular.module('itemDetailsController', [uirouter, 'ngMaterial', 'ngRoute','jkAngularRatingStars'])
     .controller('itemDetailsController', itemDetailsController)
     .config(routing)
     .name;
