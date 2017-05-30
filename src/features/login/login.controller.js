d97af5502adbb123c15833d1172a9729da1faebb
beatabubula <beatabubula@interia.pl>
2017-05-30 09:31:48 +0200

    add resert password view

--- src/features/login/login.controller.js
+++ src/features/login/login.controller.js
@@ -2,7 +2,7 @@
  * Created by wojciech_dymek on 21.04.17.
  */
 
-export default function LoginController(authService) {
+export default function LoginController(authService,$uibModal,$scope) {
     let vm = this;
     vm.login = function(){
         authService.login(vm.username, vm.password);
@@ -11,6 +11,40 @@ export default function LoginController(authService) {
     vm.logout = function () {
         authService.logout();
     };
+
+    vm.changePassword = function () {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'modals/resetPassword.html',
+            controller: 'LoginController',
+            controllerAs: 'vm',
+            size: 'lg'
+        })
+    }
+
+    vm.closeModal = function () {
+        location.reload();
+    }
+
+    $scope.reset = function () {
+        console.log("reset");
+        var user = {
+            "email" : $scope.user.email,
+            "password" : $scope.user.password
+        }
+    }
+
+    $scope.checkPasswords = function() {
+        $scope.form.passwordRepeated.$error.dontMatch = $scope.user.password !== $scope.password2 && !$scope.form.passwordRepeated.$error.required;
+        $scope.form.password.$error.wrongPasswordPattern = checkPasswordPattern($scope.user.password ) && !$scope.form.password.$error.required;
+    }
+
+    function checkPasswordPattern(str)
+    {
+        // at least one number, one lowercase and one uppercase letter
+        // at least six characters
+        var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+        return !re.test(str);
+    }
 
 
 }
