633d00821dbd8fcedef2c63d3e79d821613b91c7
drewniak <aleksandra.drewniak1a@gmail.com>
2017-06-03 01:18:44 +0200

    small refactor + fix rejected offers

--- src/features/exchange/waiting/waiting.controller.js
+++ src/features/exchange/waiting/waiting.controller.js
@@ -1,32 +1,3 @@
 export default function WaitingController($rootScope, $scope, $http, $location, $uibModal, moment) {
-
-    let user = $rootScope.globals.currentUser;
-
-    $scope.username = user.username;
-    $scope.currentNavItem = "waiting";
-    $scope.offers = [];
-    $scope.offerors = [];
-    $scope.offeror = {};
-
-
-
-    function getOffers () {
-        $http.get('/api/exchanges?ownerId=' + user.id )
-            .then(function (response) {
-                $scope.offers = response.data;
-
-
-            }, function (error) {
-                console.log(error);
-            });
-    }
-
-
-    $scope.showDetails = function(ownerId, offerId){
-        $location.path('/offerDetails').search('offerId', offerId);
-    }
-
-
-    getOffers();
 }
 
