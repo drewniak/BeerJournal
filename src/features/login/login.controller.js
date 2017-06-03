0684a56a1ee712e57a1a813a9987c762764d3afb
beatabubula <beatabubula@interia.pl>
2017-06-03 17:19:08 +0200

    change reset password

--- src/features/login/login.controller.js
+++ src/features/login/login.controller.js
@@ -2,7 +2,7 @@
  * Created by wojciech_dymek on 21.04.17.
  */
 
-export default function LoginController(authService,$uibModal,$scope) {
+export default function LoginController(authService,$uibModal,$scope,toastr,$http) {
     let vm = this;
     vm.login = function(){
         authService.login(vm.username, vm.password);
@@ -26,25 +26,16 @@ export default function LoginController(authService,$uibModal,$scope) {
     }
 
     $scope.reset = function () {
-        console.log("reset");
-        var user = {
-            "email" : $scope.user.email,
-            "password" : $scope.user.password
+        var email = {
+            "email" : $scope.user.email
         }
-    }
+        $http.put('/api/account/password/reset', email).then(function (res) {
+            toastr.success('New password was sent to your mail box');
+            location.reload();
+        },function (resp) {
+            toastr.error('Reset password failed','Error');
+        });
 
-    $scope.checkPasswords = function() {
-        $scope.form.passwordRepeated.$error.dontMatch = $scope.user.password !== $scope.password2 && !$scope.form.passwordRepeated.$error.required;
-        $scope.form.password.$error.wrongPasswordPattern = checkPasswordPattern($scope.user.password ) && !$scope.form.password.$error.required;
     }
-
-    function checkPasswordPattern(str)
-    {
-        // at least one number, one lowercase and one uppercase letter
-        // at least six characters
-        var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-        return !re.test(str);
-    }
-
 
 }
